78f516cf0580af3ddc5cb2a15d9130992d171450
Charlike Mike Reagent <mameto2011@gmail.com>
2018-11-06 12:04:40 +0200

    feat: implement
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-export default () => {};
+// https://circleci.com/api/v1.1/project
+//
+// /github/:owner/:repo/build?circle-token=XXXX
+// /github/:owner/:repo/follow?circle-token=XXXX
+
+import proc from 'process';
+import axios from 'axios';
+
+export default async function enableCircleCI(answers = {}, type = 'follow') {
+  const api = 'https://circleci.com/api/v1.1/';
+  const token =
+    proc.env.CIRCLECI_TOKEN ||
+    answers.circleci_token ||
+    (answers.author && answers.author.circleci_token) ||
+    answers.token;
+
+  if (!token) {
+    throw new Error('@tunnckocore/circleci: expect a CircleCI token');
+  }
+
+  let url = null;
+  if (answers.repository) {
+    url = ['project', 'github', answers.repository].concat(type);
+  } else {
+    url = [].concat(type);
+  }
+
+  url = `${api}${url.join('/')}?circle-token=${token}`;
+
+  return axios.post(url);
+}
--- test/index.js
+++ test/index.js
@@ -1,6 +1,20 @@
 import test from 'asia';
-import mod from '../src';
+import circleci from '../src';
 
-test('todo', (t) => {
-  t.strictEqual(typeof mod, 'function');
+test('should throw if not token and not repository are given', async (t) => {
+  try {
+    await circleci();
+  } catch (err) {
+    t.ok(err);
+    t.ok(err.message.includes('Request failed with status code 404'));
+  }
+});
+
+test('should throw if token not given', async (t) => {
+  try {
+    await circleci({ repository: 'tunnckoCoreLabs/circleci' });
+  } catch (err) {
+    t.ok(err);
+    t.ok(err.message.includes('circleci: expect a CircleCI token'));
+  }
 });
